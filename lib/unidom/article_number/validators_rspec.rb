03aed43962d3ff64de4af8c07245cb0ffa4735d5
Topbit Du <topbit.du@gmail.com>
2017-02-14 09:48:25 +0800

    1, Move the Vehicle Identification Number Validator spec from the Types RSpec examples manifest to the Validators RSpec examples manifest.

--- lib/unidom/article_number/validators_rspec.rb
+++ lib/unidom/article_number/validators_rspec.rb
@@ -1,1 +1,1 @@
-require 'rspec/types/unidom/article_number/vehicle_identification_number_validator_spec'
+require 'rspec/validators/unidom/article_number/vehicle_identification_number_validator_spec'
