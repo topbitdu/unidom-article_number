b975b7f0c251a4840f1f9c9d3855ce04bdb87fda
Topbit Du <topbit.du@gmail.com>
2017-03-08 12:19:55 +0800

    1, Improve the EAN-8 Barcode spec for the validations on the #code attribute.

--- lib/rspec/models/unidom/article_number/ean8_barcode_spec.rb
+++ lib/rspec/models/unidom/article_number/ean8_barcode_spec.rb
@@ -12,7 +12,26 @@ describe Unidom::ArticleNumber::Ean8Barcode, type: :model do
       code: '12345678'
     }
 
+    code_max_length = described_class.columns_hash['code'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+    it_behaves_like 'validates', model_attributes, :code,
+      {             } => 0,
+      { code: nil   } => 3,
+      { code: ''    } => 3,
+      { code: '1'   } => 1,
+      { code: 'A'   } => 2,
+      { code: '11'  } => 1,
+      { code: 'AA'  } => 2,
+      { code: '111' } => 1,
+      { code: 'AAA' } => 2,
+      { code: '1'*(code_max_length-1) } => 1,
+      { code: 'A'*(code_max_length-1) } => 2,
+      { code: '1'*code_max_length     } => 0,
+      { code: 'A'*code_max_length     } => 1,
+      { code: '1'*(code_max_length+1) } => 1,
+      { code: 'A'*(code_max_length+1) } => 2
 
   end
 
