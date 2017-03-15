9252713c0b62df6d4c1c0613049e66e42c779a43
Topbit Du <topbit.du@gmail.com>
2017-03-15 13:15:06 +0800

    1, Improve the Vehicle Identification Number spec for the validations on the #code attribute.

--- lib/rspec/models/unidom/article_number/vehicle_identification_number_spec.rb
+++ lib/rspec/models/unidom/article_number/vehicle_identification_number_spec.rb
@@ -14,6 +14,22 @@ describe Unidom::ArticleNumber::VehicleIdentificationNumber, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'validates', model_attributes, :code,
+      {             } => 0,
+      { code: nil   } => 2,
+      { code: ''    } => 2,
+      { code: '1'   } => 1,
+      { code: 'A'   } => 1,
+      { code: '11'  } => 1,
+      { code: 'AA'  } => 1,
+      { code: '111' } => 1,
+      { code: 'AAA' } => 1,
+      { code: 'LFVAA11A6X207535'   } => 1,
+      { code: 'LFVAA11A6X2075354'  } => 1,
+      { code: 'LFVAA11A6X2075355'  } => 0,
+      { code: 'LFVAA11A6X2075356'  } => 1,
+      { code: 'LFVAA11A6X20753559' } => 2
+
   end
 
 end
